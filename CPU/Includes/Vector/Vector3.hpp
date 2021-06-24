96c9a1c3aa8a37fdb507c33102b8741cd9a513d1
Yoo SeungWoo <dreamy1534@gmail.com>
2021-06-24 22:57:27 +0900

    refactor(main): Move function 'cross' to Vector3.hpp

--- CPU/Includes/Vector/Vector3.hpp
+++ CPU/Includes/Vector/Vector3.hpp
@@ -8,6 +8,17 @@
 
 namespace IllustRay {
 
+    // Cross product.
+    // Note that cross product is defined for 3D vector only.
+    template <typename T>
+    Vector3<T> cross(const Vector3<T>& v1, const Vector3<T>& v2) {
+        std::array<T, 3> container = std::array<T, 3>();
+        container[0] = v1[1] * v2[2] - v1[2] * v2[1];
+        container[1] = -(v1[0] * v2[2] - v1[2] * v2[0]);
+        container[2] = v1[0] * v2[1] - v1[1] * v2[0];
+
+        return Vector<T, 3>(container);
+    }
 }
 
 #endif //ILLUSTRAY_VECTOR3_HPP
--- CPU/Includes/Vector/VectorBase.hpp
+++ CPU/Includes/Vector/VectorBase.hpp
@@ -244,19 +244,6 @@ namespace IllustRay {
 	    return result;
 	}
 
-	// Cross product.
-	// Note that cross product is defined for 3D vector only.
-	template <typename T>
-	Vector<T, 3> cross(const Vector<T, 3>& v1, const Vector<T, 3>& v2) {
-
-	    std::array<T, 3> container = std::array<T, 3>();
-	    container[0] = v1[1] * v2[2] - v1[2] * v2[1];
-	    container[1] = -(v1[0] * v2[2] - v1[2] * v2[0]);
-	    container[2] = v1[0] * v2[1] - v1[1] * v2[0];
-
-	    return Vector<T, 3>(container);
-	}
-
 	// Reflect operator.
 	// Calculate perfect mirror reflection of passed vector.
 	template <typename T, size_t N>
